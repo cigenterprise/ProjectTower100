f8fe58d006534443e7ce62761c9e5fc92b44a04c
cigenterprise <cigetp@gmail.com>
2019-03-02 04:04:18 +0900

    [2019_03] Wall Test - Wall block with prefab

---
+++ Assets/Scripts/MainGame/DirCollider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirCollider : MonoBehaviour
+{
+    public BoxCollider2D dirCollider = null;
+    public Rigidbody2D dirRigidBody = null;
+    public bool bMovable = true;
+
+    private void Awake()
+    {
+        dirCollider = gameObject.AddComponent<BoxCollider2D>();
+        dirCollider.size = new Vector2( 1, 1 );
+        dirCollider.isTrigger = true;
+
+        dirRigidBody = gameObject.AddComponent<Rigidbody2D>();
+        dirRigidBody.bodyType = RigidbodyType2D.Static;
+        dirRigidBody.isKinematic = false;
+        dirRigidBody.gravityScale = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D( Collider2D collision )
+    {
+        if ( collision.CompareTag( "Wall" ) )
+            bMovable = false;
+    }
+
+    private void OnTriggerExit2D( Collider2D collision )
+    {
+        if ( collision.CompareTag( "Wall" ) )
+            bMovable = true;
+    }
+}
